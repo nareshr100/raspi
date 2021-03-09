c0019854826069aa11f3b6325cb5c5ff42e0614c
nareshr100 <naresh@suriar.me.uk>
2021-03-09 23:48:52 +0000

    adding statuses and adc

--- Door_Status_LCD/door_status.py
+++ Door_Status_LCD/door_status.py
@@ -2,8 +2,21 @@ import I2C_LCD_driver
 import board
 import busio
 import adafruit_ahtx0
+from ADCDevice import *
+from ADC import setup
 from time import sleep, strftime
 from datetime import datetime
+
+statuses = [
+        'working             ',
+        'in a class          ',
+        'in a meeting        ',
+        'in an exam/interview',
+        'relaxing            ',
+        'sleeping            ',
+        'not here but around ',
+        'not home            '
+        ]
 
 #  Setup LCDs
 mylcd1 = I2C_LCD_driver.lcd(0x27)
@@ -13,6 +26,9 @@ mylcd2 = I2C_LCD_driver.lcd(0x26)
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_ahtx0.AHTx0(i2c)
 
+#  setup ADC
+adc = ADCDevice()
+
 def get_cpu_temp():     # get CPU temperature and store it into file "/sys/class/thermal/thermal_zone0/temp"
     tmp = open('/sys/class/thermal/thermal_zone0/temp')
     cpu = tmp.read()
@@ -21,6 +37,29 @@ def get_cpu_temp():     # get CPU temperature and store it into file "/sys/class
  
 def get_time_now():     # get system time
     return datetime.now().strftime('%H:%M')
+
+def setup_adc():
+    global adc
+    if(adc.detectI2C(0x48)): # Detect the pcf8591.
+        adc = PCF8591()
+    elif(adc.detectI2C(0x4b)): # Detect the ads7830
+        adc = ADS7830()
+    else:
+        print("No correct I2C address found, \n"
+        "Please use command 'i2cdetect -y 1' to check the I2C address! \n"
+        "Program Exit. \n");
+        exit(-1)
+
+def get_status_from_adc(statuses):
+    
+    value = adc.analogRead(0)
+    value = int((value*len(statuses)/256))
+    #sprint(value)
+    #print(statuses[value])
+    
+    return statuses[value]
+
+setup_adc()
 
 mylcd1.backlight(0)
 mylcd2.backlight(0)
@@ -37,16 +76,19 @@ mylcd2.backlight(1)
 mylcd1.lcd_clear()
 mylcd2.lcd_clear()
 mylcd1.lcd_display_string("Naresh's Status:", 1)
-mylcd1.lcd_display_string("Working", 2)
 mylcd2.lcd_display_string("Naresh's Status:", 1)
-mylcd2.lcd_display_string("Working", 2)
 
 i = 0
 
 while True:
-    
+
     # dd/mm/YY H:M:S
     t_string = datetime.now().strftime("%H:%M")
+    
+    # get status
+    status = get_status_from_adc(statuses)
+    mylcd1.lcd_display_string(status, 2)
+    mylcd2.lcd_display_string(status, 2)
     
     if i == 0:
         # Read Temperature and Humidity
